1bd42d98615e62acdeff34691874a63a27c825ca
Léo Garcia Canelo <Lo Garcia Canelo>
2016-10-05 09:44:41 +0200

    Mise a zero

--- Site/Modules/MenuLog.php
+++
@@ -1,8 +0,0 @@
-<!-- Nav -->
-<nav id="nav">
-	<ul class="links">
-		<li><a href="index.php">Accueil</a></li>
-		<li><a href="modifCompte.php">Modifier son compte</a></li>
-		<li><a href="deco.php">Se déconnécter</a></li>
-	</ul>
-</nav>
