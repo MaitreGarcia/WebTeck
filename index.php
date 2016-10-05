28da3f77f51e435b141659e14b9e6565b00eee67
1992guillaume <guillaume.rodier@outlook.fr>
2016-10-05 11:51:01 +0200

    Commit

--- index.php
+++ index.php
@@ -50,12 +50,14 @@
 				<?php
 				if($log)
 				{
+					?>
 					<div class="image-grid">
-					<a href="creationAnnonce.php" class="image"><img src="images/pic04.png" alt="" /></a>
-					<a href="#" class="image"><img src="images/pic05.png" alt="" /></a>
-					<a href="#" class="image"><img src="images/pic06.png" alt="" /></a>
-					<a href="#" class="image"><img src="images/pic07.png" alt="" /></a>
-				</div>
+						<a href="creationAnnonce.php" class="image"><img src="images/pic04.png" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic05.png" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic06.png" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic07.png" alt="" /></a>
+					</div>
+					<?php
 				}
 				else
 				{
@@ -66,11 +68,10 @@
 		</section>
 
 		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
-
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+		<script src="assets/js/main.js"></script>
 	</body>
 </html>
